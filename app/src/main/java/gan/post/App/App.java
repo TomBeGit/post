fbc04d99c49898e0b5d46b2e48c3820448d49a52
gan <583354310@qq.com>
2015-12-09 20:58:46 +0800

    创建项目1

---
+++ app/src/main/java/gan/post/App/App.java
@@ -0,0 +1,14 @@
+package gan.post.App;
+
+import android.app.Application;
+
+/**
+ * Created by gan on 2015/12/9.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/gan/post/App/BaseActivity.java
+++ app/src/main/java/gan/post/App/BaseActivity.java
@@ -1,4 +1,4 @@
-package App;
+package gan.post.App;
 
 import android.support.v7.app.AppCompatActivity;
 
