276916abbf839c46bb35513991a44dd5c5017b41
gan <583354310@qq.com>
2015-12-11 13:40:50 +0800

    主界面和order界面

---
+++ app/src/main/java/gan/post/module/order/DistanceFragment.java
@@ -0,0 +1,26 @@
+package gan.post.module.order;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gan.post.R;
+
+public class DistanceFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_distance,null);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+}
